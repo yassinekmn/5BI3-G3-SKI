00fac6709d55aa2538f5ca8536cb6df05d864510
yassinekmn <yassine.kammoun@esprit.tn>
2024-10-03 23:44:26 +0100

    First push

---
+++ src/test/java/com/example/p5bi3g3ski/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.p5bi3g3ski;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
